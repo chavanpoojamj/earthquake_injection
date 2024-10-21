4841fcb08404f4de3d277951b1cd51808d35a14f
Delux <poojamjchavan@gmail.com>
2024-10-21 18:40:04 +0530

    API URLs for fetching monthly and daily earthquake data

--- bronze/load_historical_data_pyspark.py
+++ bronze/load_historical_data_pyspark.py
@@ -10,10 +10,17 @@ if __name__ == '__main__':
     spark = SparkSession.builder.master("local[*]").appName("Historical Load").getOrCreate()
 
     # pulling data from API
+    # API URLs for fetching monthly and daily earthquake data
 
-    url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
+    monthly_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
-    # using python request lib, fetching data from given url
-    response = fetch_api_data(url)
+    daily_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
 
-    print(response)
+
+    # Fetch last month's data using python request lib.
+    data_month = fetch_api_data(monthly_url)
+    print("Fetched last month's data:", data_month)
+
+    # Fetch last day's data using python request lib.
+    data_day = fetch_api_data(daily_url)
+    print("Fetched last day's data:", data_day)
